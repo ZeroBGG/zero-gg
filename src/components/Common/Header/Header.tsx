12b8b734d2ad869ec2fc451ab1a7c29e40bf39bc
HyungJun-Yoo <u-jun11@nate.com>
2023-01-02 18:35:44 +0900

    ✨ Feat: #3 - Header 컴포넌트 추가
    - Header 컴포넌트 디자인
    - Header 컴포넌트 페이지 이동 처리
    - index.tsx에 Header 컴포넌트 추가

---
+++ src/components/Common/Header/Header.tsx
@@ -0,0 +1,35 @@
+import styles from "./Header.module.scss";
+
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  const menus = [
+    { title: "홈", src: "/" },
+    { title: "듀오찾기", src: "/Duo" },
+    { title: "LCK", src: "/LCK" },
+  ];
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.h1}>Zero.gg</h1>
+
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          {menus.map((menu) => {
+            return (
+              <li key={menu.title} className={styles.li}>
+                <Link className={styles.a} to={menu.src}>
+                  {menu.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+
+      <div>
+        {/* home이 아니면 전적을 검색할 수 있는 커스텀 input 컴포넌트 들어갈 예정 */}
+      </div>
+    </header>
+  );
+}
