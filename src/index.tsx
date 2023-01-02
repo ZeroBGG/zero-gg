3cb6436dbb92d88e3d333b5d9f77133f5d310a97
BTSBRINGMEHERE <mynameisyyb@naver.com>
2023-01-02 21:41:46 +0900

    Feat: #6 - firebase 추가
    - firebase 파일 생성
    - .env 파일을 생성해서 보안추가
    - duo 파일에서 연결확인

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./index.css"
 
-import Main from "@/pages/Main";
-import Record from "@/pages/Record";
-import Duo from "@/pages/Duo";
-import LCK from "@/pages/LCK";
-import NotFound from "@/pages/NotFound";
+import Main from "@/pages/Main"
+import Record from "@/pages/Record"
+import Duo from "@/pages/Duo"
+import LCK from "@/pages/LCK"
+import NotFound from "@/pages/NotFound"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path={"/"} element={<Main />} />
-      <Route path={"/Record"} element={<Record />} />
-      <Route path={"/Duo"} element={<Duo />} />
-      <Route path={"/LCK"} element={<LCK />} />
-      <Route path={"*"} element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+	<BrowserRouter>
+		<Routes>
+			<Route path={"/"} element={<Main />} />
+			<Route path={"/Record"} element={<Record />} />
+			<Route path={"/Duo"} element={<Duo />} />
+			<Route path={"/LCK"} element={<LCK />} />
+			<Route path={"*"} element={<NotFound />} />
+		</Routes>
+	</BrowserRouter>
+)
