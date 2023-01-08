5fbb64aba3648e0ce8a2a3173acbb93dd92708f9
BTSBRINGMEHERE <mynameisyyb@naver.com>
2023-01-08 21:06:17 +0900

    change type name
    - 타입의 이름을 바꾸어 주었습니다.

--- src/components/Duo/utils/DuoType.ts
+++ src/components/Duo/utils/DuoType.ts
@@ -1,17 +1,17 @@
 export type DuoType = {
-	queue: string;
-	tier: string;
-	position: string;
+  queue: string;
+  tier: string;
+  position: string;
 
-	id: string;
-	password: string;
-	timeSet: string;
-	title: string;
-	memo: string;
-	nickName: string;
-	mostChamp: string[];
+  userId: string;
+  userPassword: string;
+  timeSet: string;
+  title: string;
+  memo: string;
+  nickName: string;
+  mostChamp: string[];
 };
 
-export type FindDuoType = Pick<DuoType, "queue" | "tier" | "position">;
+export type FindDuoType = Pick<DuoType, 'queue' | 'tier' | 'position'>;
 
-export type FindUserType = Pick<DuoType, "id" | "password">;
+export type FindUserType = Pick<DuoType, 'userId' | 'userPassword'>;
