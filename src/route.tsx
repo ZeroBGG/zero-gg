e6a1d0e4f14da1ed72351a08cc243881e6558286
HyungJun-Yoo <u-jun11@nate.com>
2023-01-20 12:52:42 +0900

    ✨ Feat: #37 - 소환사 전적 검색 기능 추가
    - 소환사 최근 전적 검색 api (리스트)
    - 소환사 1게임 데이터 api
    - 소환사 전적 검색 컴포넌트

--- src/route.tsx
+++ src/route.tsx
@@ -14,28 +14,28 @@ import Match from '@/components/LCK/components/Match/Match';
 import PlayerList from './components/LCK/components/Team/PlayerList/PlayerList';
 import Schedule from './components/LCK/components/Match/Schedule/Schedule';
 
-
 export default function route() {
   return (
-    <Routes>
-      <Route path={'/'} element={<Main />} />
+    <div>
+      <Routes>
+        <Route path={'/'} element={<Main />} />
 
-      <Route path={'/Record/:summonerId'} element={<Record />} />
-      <Route path={'/Duo'} element={<Duo />} />
+        <Route path={'/Record/:summonerId'} element={<Record />} />
+        <Route path={'/Duo'} element={<Duo />} />
 
+        <Route path={'/Duo'} element={<Duo />} />
+        <Route path={'/Duo/:id'} element={<DuoInfo />} />
 
-      <Route path={'/Duo'} element={<Duo />} />
-      <Route path={'/Duo/:id'} element={<DuoInfo />} />
+        <Route path={'/lck'} element={<LCK />} />
+        <Route path={'lck/team/'} element={<Team />}>
+          <Route path={':team'} element={<PlayerList />} />
+        </Route>
+        <Route path={'lck/matches/'} element={<Match />}>
+          <Route path={':month'} element={<Schedule isHover={false} />} />
+        </Route>
 
-      <Route path={'/lck'} element={<LCK />} />
-      <Route path={'lck/team/'} element={<Team />}>
-        <Route path={':team'} element={<PlayerList />} />
-      </Route>
-      <Route path={'lck/matches/'} element={<Match />}>
-        <Route path={':month'} element={<Schedule isHover={false} />} />
-      </Route>
-
-      <Route path={'*'} element={<NotFound />} />
-    </Routes>
+        <Route path={'*'} element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
