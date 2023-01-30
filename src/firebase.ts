1024f55b4e8583b6bf9a46651a594d12d5512fce
BTSBRINGMEHERE <mynameisyyb@naver.com>
2023-01-30 18:18:41 +0900

    firebase pwa result
    - vite는 firebase의 pwa를 사용할 수 없음... 경로문제 같음 해당 경로의 파일을 찾을 수 없고 404ERROR가 나옴 ㅠㅠ

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore, addDoc, collection } from 'firebase/firestore';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const {
@@ -12,10 +11,19 @@ const {
   VITE_FIREBASE_SENDER_ID,
   VITE_FIREBASE_APP_ID,
   VITE_FIREBASE_DATABASE_URL,
-  VITE_FIREBASE_VAPID_KEY,
 } = import.meta.env;
 
-const firebaseConfig = {
+type FirebaseType = {
+  readonly apiKey: string;
+  readonly authDomain: string;
+  readonly projectId: string;
+  readonly storageBucket: string;
+  readonly messagingSenderId: string;
+  readonly appId: string;
+  readonly databaseURL: string;
+};
+
+const firebaseConfig: FirebaseType = {
   apiKey: VITE_FIREBASE_API_KEY,
   authDomain: VITE_FIREBASE_AUTH_DOMAIN,
   projectId: VITE_FIREBASE_PROJECT_ID,
@@ -23,35 +31,8 @@ const firebaseConfig = {
   messagingSenderId: VITE_FIREBASE_SENDER_ID,
   appId: VITE_FIREBASE_APP_ID,
   databaseURL: VITE_FIREBASE_DATABASE_URL,
-  vapidKey: VITE_FIREBASE_VAPID_KEY,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const dbService = getFirestore();
-
-const messaging = getMessaging(app);
-
-const requestPermisstion = async () => {
-  console.log('권한 요청중...');
-  const permission = await Notification.requestPermission();
-  if (permission === 'denied') {
-    console.log('권한 허용 안됨..');
-    return;
-  }
-
-  console.log('권한 요청 허용됨');
-
-  const token = await getToken(messaging, {
-    vapidKey: VITE_FIREBASE_VAPID_KEY,
-  });
-
-  if (token) console.log('token: ', token);
-  else console.log('토큰 ㅇㄷ?');
-
-  onMessage(messaging, (payload) => {
-    console.log('메세지 도착: ', payload);
-  });
-};
-
-requestPermisstion();
