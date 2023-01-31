72837b040887d73906a08a5ec076c92ce6c3aeb0
BTSBRINGMEHERE <mynameisyyb@naver.com>
2023-01-31 15:23:48 +0900

    build 에러 수정
    빌드청크사이즈 확대

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), VitePWA()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 });
