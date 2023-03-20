281ecaea76ad2cc9b2d8dec15535b847fff6f098
HyungJun-Yoo <u-jun11@nate.com>
2023-03-21 05:06:45 +0900

    🎨 Refactor: #105 - Lighthouse 성능 개선

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,12 +3,13 @@ import InputID from './InputID/InputID';
 import Circle from './Circle/Circle';
 
 import bg from '/assets/images/Main/bg.mp4';
+import poster from '/assets/images/Main/bg.webp';
 
 export default function Main() {
   return (
     <main className={styles.main}>
       <div className={styles.wrap}>
-        <video className={styles.video} src={bg} loop autoPlay muted></video>
+        <video className={styles.video} src={bg} poster={poster} loop autoPlay muted></video>
         <Circle />
       </div>
 
